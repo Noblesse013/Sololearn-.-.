99886abd181ce10d3a23c8945540c804bca076f0
Mehreen Mallick Fiona <155657140+Noblesse013@users.noreply.github.com>
2024-03-14 21:52:47 +0600

    Update map().py

--- Python Intermediate/map().py
+++ Python Intermediate/map().py
@@ -1,1 +1,11 @@
+'''
+The map() function applies a specified function to every element in an iterable, like lists or tuples. 
+It produces a result that can be transformed into a list using the list() function for easy viewing or further use.
+'''
+names = ["alice", "bob", "CHARLIE", "Tom"]
 
+def capitalize(name):
+  return name.capitalize()
+
+capitalized = map(capitalize, names)
+capitalized = list(capitalized)
