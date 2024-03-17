a5f051f34b7df0fa1946099e2b812dcab62f9b2d
Mehreen Mallick Fiona <155657140+Noblesse013@users.noreply.github.com>
2024-03-17 22:41:29 +0600

    Delete OPP directory

--- OPP/Class & Object.py
+++
@@ -1,9 +0,0 @@
-'''The __init__ function must be defined before you may add attributes to a class. The first parameter to this function is always self, which represents the class's instance. '''
-class Car():
-  def __init__(self, brand, color):
-    self.brand = brand  #attribute
-    self.color = color
-
-my_car = Car('Tesla', 'orange') #object
-
-
